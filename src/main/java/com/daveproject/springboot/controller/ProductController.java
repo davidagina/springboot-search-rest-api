f6a19866219cf97af80b8ee2b1069c03c84ec589
davidagina <davidagina64@gmail.com>
2023-10-18 02:23:50 +0100

    Added ProductController class

---
+++ src/main/java/com/daveproject/springboot/controller/ProductController.java
@@ -0,0 +1,30 @@
+package com.daveproject.springboot.controller;
+
+import com.daveproject.springboot.entity.Product;
+import com.daveproject.springboot.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam("query") String query){
+        return ResponseEntity.ok(productService.searchProducts(query));
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product){
+        return productService.createProduct(product);
+    }
+}
